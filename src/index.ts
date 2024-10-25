7a419b0837ede1694e8b67847daaf74f3911c6ae
Guilherme.fadel <guilherme.fadel@becomex.com.br>
2024-10-25 10:37:50 -0300

    vercel fix

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { router } from './router';
 import * as cors from 'cors';
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
+import http from 'http';
 
 dotenv.config();
 
@@ -40,6 +41,7 @@ mongoose
     app.use(cors(corsOptions));
     app.use(express.json());
     app.use(router);
+    app.use(express.static("public")) 
 
     app.listen(port, () => {
       console.log('Server is listening on port 3000');
